e92b4c3a891249aaa02ba30b836a1df6d073d437
Ondrej Zara <ondrej.zara@gmail.com>
2014-03-10 21:49:07 +0100

    descriptions, bubbles

---
+++ js/cell.bubble.js
@@ -0,0 +1,18 @@
+Cell.Bubble = function() {
+	Cell.call(this, {
+		ch: "o",
+		fg: [40, 40, 255]
+	}, false);
+}
+Cell.Bubble.extend(Cell);
+
+Cell.Bubble.prototype.enter = function(being) {
+	var o2 = being.getStat("o2");
+	var maxo2 = being.getStat("maxo2");
+	
+	if (o2 < maxo2) {
+		being.adjustStat("o2", Math.min(Rules.BUBBLE_RESTORE, maxo2-o2));
+		if (being == Game.player) { Game.text.write("You restore a bit of oxygen from this bubble."); }
+		this._level.removeCell(this._xy);
+	}
+}
