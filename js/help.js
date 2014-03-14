5529538233e1796afc3558c5735b32fa7011ced7
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2014-03-14 09:30:54 +0100

    help + texts

---
+++ js/help.js
@@ -0,0 +1,78 @@
+var Help = function(first) {
+	this._first = first;
+	this._promise = new Promise();
+}
+
+Help.prototype.show = function() {
+	Game.display.clear(); /* clear all */
+
+	if (!this._first) { Game.status.update(); }
+	this._draw();
+
+	window.addEventListener("keydown", this);
+
+	return this._promise;
+}
+
+Help.prototype.handleEvent = function(e) {
+	if (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey) { return; }
+	if (e.keyCode == ROT.VK_F11 || e.keyCode == ROT.VK_F5) { return; }
+	if (this._first || e.keyCode == 27 || e.keyCode == ROT.VK_Z) {
+		e.preventDefault();
+		this._hide();
+		this._promise.fulfill();
+	}
+}
+
+Help.prototype._hide = function() {
+	window.removeEventListener("keydown", this);
+	Game.text.clear();
+}
+
+Help.prototype._draw = function() {
+	Game.text.clear();
+	if (this._first) {
+		Game.text.write("Press %c{#fff}any key%c{} to start the game.");
+	} else {
+		Game.text.write("Press %c{#fff}Escape%c{} or %c{#fff}Z%c{} to return back to game.");
+	}
+	Game.text.flush();
+
+	var x = 0, y = Game.TEXT_HEIGHT;
+	Game.display.drawText(x, y, "• To move around (8 directions), use %c{#fff}arrows%c{}, %c{#fff}numpad%c{} or %c{#fff}vim-keys%c{}.");
+	y += 2;
+	Game.display.drawText(x, y, "• To attack, move towards your target. To enter tunnels and pick items, press %c{#fff}Enter%c{}.");
+	y += 2;
+	Game.display.drawText(x, y, "• Press %c{#fff}i%c{} to open your inventory, %c{#fff}q%c{} to view quests, %c{#fff}?%c{} for this help screen.");
+	y += 2;
+	Game.display.drawText(x, y, "• Switch to fullscreen (%c{#fff}F11%c{}) for larger font.");
+
+	y += 3;
+	var x1 = 3, x2 = x1 + Math.round(Game.MAP_SIZE.x/3), x3 = x2 + Math.round(Game.MAP_SIZE.x/3);
+
+	Game.display.drawText(x1, y, "%h".format(Cell.grass));
+	Game.display.drawText(x2, y, "%c{#66f}≈%c{} water");
+	Game.display.drawText(x3, y, "%h".format(Cell.shore));
+	y += 1;
+	Game.display.drawText(x1, y, "%h".format(Cell.wall));
+	Game.display.drawText(x2, y, "%h".format(new Cell.Bubble()));
+	Game.display.drawText(x3, y, "%h".format(new Cell.Seaweed()));
+	y += 1;
+	Game.display.drawText(x1, y, "%h".format(Game.player));
+	Game.display.drawText(x2, y, "%c{#fff}<>%c{} underwater tunnel");
+	Game.display.drawText(x3, y, "%c{#fff}?%c{} an item");
+
+	y += 3;
+	Game.display.drawText(x1, y, "%h".format(new Being.Seahorse()));
+	Game.display.drawText(x2, y, "%h".format(new Being.Crab()));
+	Game.display.drawText(x3, y, "%h".format(new Being.Starfish()));
+	y += 1;
+	Game.display.drawText(x1, y, "%h".format(new Being.Jellyfish()));
+	Game.display.drawText(x2, y, "%h".format(new Being.Piranha()));
+	Game.display.drawText(x3, y, "%h".format(new Being.Octopus()));
+	y += 1;
+	Game.display.drawText(x1, y, "%h".format(new Being.Snake()));
+	Game.display.drawText(x2, y, "%h".format(new Being.LargeSnake()));
+	Game.display.drawText(x3, y, "%h".format(new Being.Swordfish()));
+
+}
